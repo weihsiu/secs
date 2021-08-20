700992923fd537b80773d29c3904ba810a3a9c50
Walter Chang <weihsiu@gmail.com>
2021-08-20 12:39:53 +0800

    doing some tests on the component design

--- core/src/main/scala/secs/Component.scala
+++ core/src/main/scala/secs/Component.scala
@@ -1,4 +1,23 @@
 package secs
 
+import scala.compiletime.*
+
 trait Component
 
+trait ComponentMeta[A <: Component]:
+  def name: String
+
+case class Dimension(width: Double, height: Double) extends Component
+given ComponentMeta[Dimension] with
+  val name = "Dimension"
+
+case class Heading(angle: Double) extends Component
+given ComponentMeta[Heading] with
+  val name = "Heading"
+
+def test[A <: Component: ComponentMeta]: String =
+  summon[ComponentMeta[A]].name
+
+@main
+def componentTest() =
+  println(test[Heading])
