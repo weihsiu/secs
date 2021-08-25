078a194cb5a4371c4c5076a9b87e6063a60c6f20
Walter Chang <weihsiu@gmail.com>
2021-08-25 18:19:07 +0800

    thinking about runtime summoning

--- core/src/main/scala/secs/Entity.scala
+++ core/src/main/scala/secs/Entity.scala
@@ -2,3 +2,5 @@ package secs
 
 opaque type Entity = Int
 
+object Entity:
+  def apply(entity: Int): Entity = entity
--- core/src/main/scala/secs/World.scala
+++ core/src/main/scala/secs/World.scala
@@ -1,10 +1,33 @@
 package secs
 
 trait World:
-  def entitiesWithComponent[C <: Component]: Set[Entity]
-  def componentsInEntity(entity: Entity): Set[Component]
+  def entitiesWith[C <: Component : ComponentMeta]: Set[Entity]
+  def componentsWithin(entity: Entity): Set[Component]
   def spawnEntity(): Entity
   def despawnEntity(entity: Entity): Unit
-  def insertComponent(entity: Entity, component: Component): Unit
-  def updateComponent(entity: Entity, update: Component => Component): Unit
-  def removeComponent(entity: Entity, component: Component): Unit
+  def insertComponent[C <: Component : ComponentMeta](entity: Entity, component: C): Unit
+  def updateComponent[C <: Component : ComponentMeta](entity: Entity, update: C => C): Unit
+  def removeComponent[C <: Component : ComponentMeta](entity: Entity, component: C): Unit
+
+given World with
+  private var components = Map.empty[String, Set[Entity]]
+  private var entities = Map.empty[Entity, Set[Component]]
+  private var currentEntityId = 0
+  def entitiesWith[C <: Component : ComponentMeta] = components.get(summon[ComponentMeta[C]].name).getOrElse(Set.empty)
+  def componentsWithin(entity: Entity) = entities.get(entity).getOrElse(Set.empty)
+  def spawnEntity() =
+    val entity = Entity(currentEntityId)
+    entities += entity -> Set.empty
+    currentEntityId += 1
+    entity
+  def despawnEntity(entity: Entity) =
+    entities.get(entity).foreach(_.foreach { c =>
+      summon[ComponentMeta[]]
+      removeComponent(entity, c)
+    })
+    entities -= entity
+  def insertComponent[C <: Component : ComponentMeta](entity: Entity, component: C) =
+    entities = entities.updatedWith(entity)(_.map(_ + component))
+    components = components.updatedWith(summon[ComponentMeta[C]].name)(_.map(_ + entity))
+  def updateComponent[C <: Component : ComponentMeta](entity: Entity, update: C => C) = ???
+  def removeComponent[C <: Component : ComponentMeta](entity: Entity, component: C) = ???
