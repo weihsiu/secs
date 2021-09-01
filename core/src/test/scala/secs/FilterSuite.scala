0fa03737d31bc0d50111c608a2e887b402ad85f5
Walter Chang <weihsiu@gmail.com>
2021-09-01 12:29:32 +0800

    experimenting with filter

--- core/src/test/scala/secs/FilterSuite.scala
+++ core/src/test/scala/secs/FilterSuite.scala
@@ -1,16 +1,40 @@
 package secs
 
+import scala.compiletime.*
+
 class FilterSuite extends munit.FunSuite:
   enum BoolOps:
-    case ¬[A <: BoolOps | Component]()
-    case ∧[A <: BoolOps | Component, B <: BoolOps | Component]()
-    case ∨[A <: BoolOps | Component, B <: BoolOps | Component]()
+    case ¬[A <: BoolOps | Component](x: A | ComponentMeta[?])
+    case ∧[A <: BoolOps | Component, B <: BoolOps | Component](
+        x: A | ComponentMeta[?],
+        y: B | ComponentMeta[?]
+    )
+    case ∨[A <: BoolOps | Component, B <: BoolOps | Component](
+        x: A | ComponentMeta[?],
+        y: B | ComponentMeta[?]
+    )
   import BoolOps.*
 
-  ¬[Dimension ∧ (Heading ∨ ¬[Rotation])]()
+  given [A <: Component: ComponentMeta]: ¬[A] =
+    ¬[A](summon[ComponentMeta[A]])
+
+  given [A <: Component: ComponentMeta, B <: Component: ComponentMeta]
+      : ∧[A, B] =
+    ∧[A, B](summon[ComponentMeta[A]], summon[ComponentMeta[B]])
+
+  given [A <: Component: ComponentMeta, B <: Component: ComponentMeta]
+      : ∨[A, B] =
+    ∨[A, B](summon[ComponentMeta[A]], summon[ComponentMeta[B]])
 
   trait Filter[OS <: BoolOps]:
-    def boolOps: OS
+    inline def boolOps: OS
 
-  given [OS <: BoolOps]: Filter[OS] with
-    def boolOps = ???
+  inline given [OS <: BoolOps]: Filter[OS] with
+    inline def toBoolOps[OS <: BoolOps]: BoolOps =
+      inline erasedValue[OS] match
+        case _: ¬[Component] => ¬(summon[ComponentMeta[]])
+    inline def boolOps = ???
+
+  inline def filter[OS <: BoolOps](using F: Filter[OS]): Unit = ???
+
+  filter[¬[Dimension ∧ (Heading ∨ ¬[Rotation])]]
