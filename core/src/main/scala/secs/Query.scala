7fa4fc09ecafd616b4f4863fb89f7070d74f0493
Walter Chang <weihsiu@gmail.com>
2021-09-09 12:17:53 +0800

    trying option query

--- core/src/main/scala/secs/Query.scala
+++ core/src/main/scala/secs/Query.scala
@@ -1,8 +1,9 @@
 package secs
 
-import scala.Tuple.{Filter as _, *}
-import scala.compiletime.*
 import secs.BoolOps.*
+
+import scala.Tuple.{Filter as TFilter, *}
+import scala.compiletime.*
 
 trait Query[CS <: Tuple, OS <: BoolOps]:
   inline def result: List[CS]
@@ -10,8 +11,15 @@ trait Query[CS <: Tuple, OS <: BoolOps]:
 type Query1[CS <: Tuple] = Query[CS, ¬[¬[EntityC]]]
 
 object Query:
-  inline given [CS <: Tuple, OS <: BoolOps](using W: World): Query[CS, OS]
-    with
+  inline given [CS <: Tuple, OS <: BoolOps](using W: World): Query[CS, OS] with
+    type NotOption[C] <: Boolean = C match
+      case Option[?] => false
+      case ?         => true
+
+    type NoOptionCM[CM] = CM match
+      case ComponentMeta[Option[c]] => ComponentMeta[c]
+      case ComponentMeta[c]         => ComponentMeta[c]
+
     type ToComponent[CM] = CM match
       case ComponentMeta[c] => c
 
@@ -26,31 +34,39 @@ object Query:
       cms.map[ToComponent](
         [cm] =>
           (x: cm) =>
-            W.componentsWithin(entity)(x.asInstanceOf[ComponentMeta[Component]]).asInstanceOf[ToComponent[cm]]
+            x match
+              case _: ComponentMeta[Option[? <: Component]] =>
+                W.componentsWithin(entity)
+                  .get(summon[NoOptionCM[cm]].asInstanceOf[ComponentMeta[Component]])
+                  .asInstanceOf[ToComponent[cm]]
+              case _ =>
+                W.componentsWithin(entity)(x.asInstanceOf[ComponentMeta[Component]])
+                  .asInstanceOf[ToComponent[cm]]
       )
 
     private def validEntity(boolOps: BoolOps)(entity: Entity): Boolean =
       val cs = W.componentsWithin(entity)
-      def hasComponent(cm: ComponentMeta[?]): Boolean = cs.contains(cm.asInstanceOf[ComponentMeta[Nothing]])
+      def hasComponent(cm: ComponentMeta[?]): Boolean =
+        cs.contains(cm.asInstanceOf[ComponentMeta[Nothing]])
       boolOps match
         case ¬(cm: ComponentMeta[?]) => !hasComponent(cm)
-        case ¬(os: BoolOps) => !validEntity(os)(entity)
-        case ∧(cm1: ComponentMeta[?], cm2: ComponentMeta[?]) => hasComponent(cm1) && hasComponent(cm2)
+        case ¬(os: BoolOps)          => !validEntity(os)(entity)
+        case ∧(cm1: ComponentMeta[?], cm2: ComponentMeta[?]) =>
+          hasComponent(cm1) && hasComponent(cm2)
         case ∧(cm: ComponentMeta[?], os: BoolOps) => hasComponent(cm) && validEntity(os)(entity)
         case ∧(os: BoolOps, cm: ComponentMeta[?]) => hasComponent(cm) && validEntity(os)(entity)
         case ∧(os1: BoolOps, os2: BoolOps) => validEntity(os1)(entity) && validEntity(os2)(entity)
-        case ∨(cm1: ComponentMeta[?], cm2: ComponentMeta[?]) => hasComponent(cm1) || hasComponent(cm2)
+        case ∨(cm1: ComponentMeta[?], cm2: ComponentMeta[?]) =>
+          hasComponent(cm1) || hasComponent(cm2)
         case ∨(cm: ComponentMeta[?], os: BoolOps) => hasComponent(cm) || validEntity(os)(entity)
         case ∨(os: BoolOps, cm: ComponentMeta[?]) => hasComponent(cm) || validEntity(os)(entity)
         case ∨(os1: BoolOps, os2: BoolOps) => validEntity(os1)(entity) || validEntity(os2)(entity)
-        case _ => sys.error("invalid BoolOps")
+        case _                             => sys.error("invalid BoolOps")
 
     inline def result =
       val boolOps = summon[Filter[OS]].boolOps
-      val r = toEntities(summonAll[Map[CS, ComponentMeta]])
+      val r = toEntities(summonAll[Map[TFilter[CS, NotOption], ComponentMeta]])
       r.reduce(_.intersect(_))
         .toList
         .filter(validEntity(boolOps))
-        .map(e =>
-          toComponents(e, summonAll[Map[CS, ComponentMeta]]).asInstanceOf[CS]
-        )
+        .map(e => toComponents(e, summonAll[Map[CS, ComponentMeta]]).asInstanceOf[CS])
